c329985a441741f96219efc8aa326082edccba15
kharshaw <keith.harshaw@gmail.com>
2021-01-26 13:20:06 -0500

    Add eslint support Add winston logging. Fixed lint errors. Refactoring into functions.

--- src/index.js
+++ src/index.js
@@ -1,42 +1,47 @@
 import https from 'https';
 import fs from 'fs';
 import dotenv from 'dotenv';
-
+import winston from 'winston';
 
 dotenv.config();
 
-const username = process.env.USERNAME;
-const password = process.env.PASSWORD;
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: {service: 'login-log-stats'},
+    transports: [
+        new winston.transports.File({filename: 'error.log', level: 'error'}),
+        new winston.transports.File({filename: 'combined.log'}),
+    ],
+});
 
-const token = `Basic ${Buffer.from(username + ':' + password).toString('base64')}`;
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple(),
+    }));
+}
 
-let startDate = '2021/01/23';
-let endDate = '2021/01/31;'
-
-
-async function runQuery(term, begin, end) {
-
+async function runQueryAsync(term, begin, end, token) {
     let rsid = '';
-    let query = encodeURI(`q="${term}"&from=${begin}&until=${end}`);
+    const query = encodeURI(`q="${term}"&from=${begin}&until=${end}`);
 
 
     const options = {
         hostname: `myfrontline.loggly.com`,
         path: `/apiv2/search?${query}&size=1000`,
-        method: "GET",
-        headers: { Authorization : token}
-        
+        method: 'GET',
+        headers: {Authorization: token},
+
     };
 
     console.log(`searching: https://${options.hostname}${options.path}`);
 
     const request = new Promise((resolve, reject) => {
         const req = https.request(options, (res) => {
-
             let data = '';
             console.log(`status: ${res.statusCode}`);
-                
-            res.on('data', d => {
+
+            res.on('data', (d) => {
                 data += d.toString();
             });
 
@@ -44,204 +49,248 @@ async function runQuery(term, begin, end) {
                 if (res.statusCode === 200) {
                     rsid = JSON.parse(data).rsid.id;
                     resolve(rsid);
+                } else {
+                    // eslint-disable-next-line max-len
+                    reject(new Error({status: res.statusCode, message: res.statusMessage || 'error'}));
                 }
-                else {
-                    reject({ status: res.statusCode, message: res.statusMessage || 'error'});
-                }
-            })
+            });
         });
 
         req.end();
 
-        req.on('error', e => {
+        req.on('error', (e) => {
             reject(e);
         });
     });
 
     try {
         rsid = await request;
-    }
-    catch(error) {
+    } catch (error) {
         rsid = error;
     }
 
     return rsid;
 }
 
-async function getEvents(rsid, page) {
-
+async function getEventsAsync(rsid, page, token) {
     let events = '';
 
     const request = new Promise((resolve, reject) => {
-
         const req = https.request({
-            hostname: "myfrontline.loggly.com",
+            hostname: 'myfrontline.loggly.com',
             port: 443,
             path: `/apiv2/events?rsid=${rsid}&page=${page}`,
-            method: "GET",
-            headers: { Authorization : token}
-        }, res => {
-    
+            method: 'GET',
+            headers: {Authorization: token},
+        }, (res) => {
             let data ='';
-        
-    
-            res.on('data', d => {
+
+
+            res.on('data', (d) => {
                 data += d.toString();
             });
-    
+
             res.on('end', () => {
                 if (res.statusCode !== 200) {
-                    reject({ status: res.statusCode, message: res.statusMessage || 'error'});
+                    // eslint-disable-next-line max-len
+                    reject(new Error({status: res.statusCode, message: res.statusMessage || 'error'}));
                 } else {
-
                     resolve(JSON.parse(data));
                 }
             });
-        }); 
-        
-        req.on('error', e => {
+        });
+
+        req.on('error', (e) => {
             reject(e);
         });
-        
+
         req.end();
-    
     });
 
     try {
         events = await request;
-    }
-    catch(error) {
+    } catch (error) {
         console.log(error.message);
         events = error;
     }
-    
-    return events;
 
+    return events;
 };
 
-const getDailyEvents = async (date, username, password) => {
+const getDailyEventsAsync = async (dateToProcess, token) => {
+    const dateToProcessEnd = new Date(dateToProcess);
+    dateToProcessEnd.setHours(23, 59, 59, 999);
 
+    let rsid = await runQueryAsync('Login Success',
+        dateToProcess.toISOString(),
+        dateToProcessEnd.toISOString(), token);
+    let page = 0;
 
-}
+    // eslint-disable-next-line max-len
+    logger.info(`RSID: ${rsid} start: ${dateToProcess.toISOString()} end: ${dateToProcessEnd.toISOString()}`);
 
-(async function() {
+    let pageData;
 
-    let totalLogins = 0;
-    let totalEvents = 0;
-    let totalBadEvents = 0;
+    const logins = {};
 
-    let start = '2021/1/1';
-    let end = '2021/1/31';
+    do {
+        try {
+            pageData = await getEventsAsync(rsid, page, token);
+        } catch (e) {
+            logger.error(e);
+            logger.info(`getEvents error: ${JSON.stringify}`);
+            throw e;
+        }
 
-    let endDate = new Date(end);
-    endDate.setHours(23,59,59,999);
+        console.log(`processing page: ${page}`);
+        if (pageData && pageData.events) {
+            pageData.events.forEach((e) => {
+                if (e.event.json.Name) {
+                    let username;
 
-    let currentDate = new Date(start);
-    currentDate.setHours(0,0,0,0);
-
-  
-    while (currentDate < endDate) {
-        let logins = {};
-        let pageData;
-
-        let currentEndDate = new Date(currentDate);
-        currentEndDate.setHours(23,59,59,999);
-
-        let rsid = await runQuery("Login Success", currentDate.toISOString(), currentEndDate.toISOString());
-        let page = 0;
-
-        console.log(`RSID: ${rsid} start: ${currentDate.toISOString()} end: ${currentEndDate.toISOString()}`);
-     
-        do {
-
-            try {
-                pageData = await getEvents(rsid, page);
-            }
-            catch (e) {
-                console.log(`error: ${JSON.stringify}`);
-                throw e;
-            }
-
-            console.log(`processing page: ${page}`);
-            if (pageData && pageData.events) {
-        
-                pageData.events.forEach(e => {
-    
-                    if (e.event.json.Name) {
-    
-                        let username;
-    
-                        switch (e.event.json.Name) {
-                            case "Local Login Success":
-                                username = e.event.json.details.LoginUserName;
-                                if (!logins.hasOwnProperty(username)) {
-                                    logins[username] = [];
-                                }
-    
-                                totalLogins++;
-    
-                                break;
-                            case "External Login Success":
-                                username = `${e.event.json.details.Provider}|${e.event.json.details.ProviderId}`;
-                                if (!logins.hasOwnProperty(username)) {
-                                    logins[username] = [];
-                                }
-                                
-                                totalLogins++;
-                                
-                                break;
-                            default:
-                                console.log(`Unexpected event: ${e.event.json.Name}`);
-                                totalBadEvents++;
-                                break;
+                    switch (e.event.json.Name) {
+                    case 'Local Login Success':
+                        username = e.event.json.details.LoginUserName;
+                        if (!logins.hasOwnProperty(username)) {
+                            logins[username] = [];
                         }
-            
-                        if (username) logins[username].push({ timestamp: e.event.json.timestamp, clientID: e.event.json.details.SignInMessage.ClientId });
-                        
+
+                        break;
+                    case 'External Login Success':
+                        // eslint-disable-next-line max-len
+                        username = `${e.event.json.details.Provider}|${e.event.json.details.ProviderId}`;
+                        if (!logins.hasOwnProperty(username)) {
+                            logins[username] = [];
+                        }
+
+                        break;
+                    default:
+                        logger.warn(`Unexpected event: ${e.event.json.Name}`);
+                        break;
                     }
-                    totalEvents++;
-                });
-            }
-            
-            if (!pageData.events) {
-                //
-                // probably the RSID expired????  run query again and try to reprocess page
-                //
-                try {
-                console.log(pageData);
-                console.log(`rsid not found.  get it again: `);
-                rsid = await runQuery("Login Success", currentDate.toISOString(), currentEndDate.toISOString());                
-                pageData = await getEvents(rsid, page);
+
+                    // eslint-disable-next-line max-len
+                    if (username) logins[username].push({timestamp: e.event.json.timestamp, clientID: e.event.json.details.SignInMessage.ClientId});
                 }
-                catch(e)
-                {
-                    console.log(`error: ${JSON.stringify(e)}`);
-                }
+            });
+
+            page++;
+        } else {
+            logger.debug(pageData);
+            logger.info(`rsid not found.  get it again `);
+            //
+            // probably the RSID expired????
+            // run query again and try to reprocess page
+            //
+            try {
+                rsid = await runQueryAsync('Login Success',
+                    dateToProcess.toISOString(),
+                    dateToProcessEnd.toISOString(), token);
+            } catch (e) {
+                logger.error(e);
             }
-            else {
-                page++;
-            }
-        
-        } while (pageData.events && pageData.events.length > 0);
-        
+        }
+    } while (pageData.events && pageData.events.length > 0);
 
-        console.log(`processing day ${currentDate.toISOString()} to ${currentEndDate.toISOString()}`);
-        
-        let dayFile =  `.\\data\\logins.${currentDate.getFullYear()}_${currentDate.getMonth()+1}_${currentDate.getDate()}.json`;
-        fs.writeFileSync(dayFile, JSON.stringify(logins));
-    
-        console.log(`unique logins: ${Object.keys(logins).length}`);
-        console.log(`total logins: ${totalLogins}`);
-        console.log(`total events: ${totalEvents}`);
-        console.log(`total bad events: ${totalBadEvents}`);
+    return logins;
+};
 
-        currentDate.setDate(currentDate.getDate() + 1);
+const getAuthTokenFromEnvironment = () => {
+    const username = process.env.LOGGLY_USERNAME || '';
+    const password = process.env.LOGGLY_PASSWORD || '';
 
+
+    if (!username.length || !password.length) {
+        // eslint-disable-next-line max-len
+        const message = 'The environment must specify LOGGLY_USERNAME and LOGGLY_PASSWORD';
+        logger.warn(message);
+        throw (message);
     }
 
-    
-    
-    
-    
+    return `Basic ${Buffer.from(username + ':' + password).toString('base64')}`;
+};
 
+const displayUsage = (messages) => {
+    // eslint-disable-next-line max-len
+    console.log(`Usage ${process.argv[0]} ${process.argv[1]} options\r\n\t--start {startDate}\r\n\t--end {endDate}`);
+
+    messages.forEach((m) => {
+        console.log(`\tMessage: ${m}`);
+    });
+};
+
+const processCommandLine = () => {
+    const errors = [];
+
+    const startArg = process.argv.indexOf('--start');
+    let startDate = {};
+
+    if (startArg >= 0) {
+        const startString = process.argv[startArg+1];
+        if (Date.parse(startString)) {
+            startDate = new Date(startString);
+        } else {
+            errors.push(`Start date must be a valid date`);
+        }
+    } else {
+        errors.push('Start date is a required command line argument');
+    }
+
+    const endArg = process.argv.indexOf('--end');
+    let endDate = {};
+
+    if (endArg >= 0) {
+        const endString = process.argv[endArg+1];
+
+        if (Date.parse(endString)) {
+            endDate = new Date(endString);
+        } else {
+            errors.push(`end date must be a valid date`);
+        }
+    } else {
+        errors.push('End date is a required command line argument');
+    }
+
+    return {startDate, endDate, errors};
+};
+
+(async function() {
+    const {startDate, endDate, errors} = processCommandLine();
+
+    if (errors.length > 0) {
+        errors.forEach((e) => logger.error(e));
+
+        displayUsage(errors);
+        return 1;
+    }
+
+    endDate.setHours(23, 59, 59, 999);
+    startDate.setHours(0, 0, 0, 0);
+
+    const token = getAuthTokenFromEnvironment();
+
+    // eslint-disable-next-line max-len
+    logger.info(`start:\t${startDate.toString()} \r\nend:\t${endDate.toString()} \r\ntoken:\t${token}`);
+
+    const currentDate = new Date(startDate);
+
+    while (currentDate < endDate) {
+        let logins = {};
+
+        // eslint-disable-next-line max-len
+        const dayFile = `..\\data\\logins.${currentDate.getFullYear()}_${currentDate.getMonth()+1}_${currentDate.getDate()}.json`;
+
+        if (!fs.existsSync(dayFile)) {
+            logins = await getDailyEventsAsync(currentDate, token);
+
+            // eslint-disable-next-line max-len
+            logger.info(`processing day ${currentDate.toISOString()} to ${currentEndDate.toISOString()}`);
+
+            fs.writeFileSync(dayFile, JSON.stringify(logins));
+        } else {
+            logger.info(`outfile exists for ${currentDate.toISOString()}`);
+        }
+
+        currentDate.setDate(currentDate.getDate() + 1);
+    };
 })();
