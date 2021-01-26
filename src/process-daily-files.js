dea6f27d7afac0d28219305a656169af034529dd
kharshaw <keith.harshaw@gmail.com>
2021-01-26 13:20:06 -0500

    Add eslint support Add winston logging. Fixed lint errors. Refactoring into functions.

--- src/process-daily-files.js
+++ src/process-daily-files.js
@@ -1,57 +1,56 @@
 import fs from 'fs';
 
 (async function() {
+    const startDate = new Date('2021/01/01');
+    const endDate = new Date('2021/01/31');
 
-    let startDate = new Date('2021/01/01');
-    let endDate = new Date('2021/01/31');
+    const currentDate = new Date(startDate);
+    currentDate.setHours(0, 0, 0, 0);
 
-    let currentDate = new Date(startDate);
-    currentDate.setHours(0,0,0,0);
-
-    var logins = {};
-    var totalLogins = 0;
-    var uniqueLogins = 0;
+    const logins = {};
+    let totalLogins = 0;
+    let uniqueLogins = 0;
 
     while (currentDate <= endDate) {
-
-        let filename = `..\\data\\logins.${currentDate.getFullYear()}_${currentDate.getMonth()+1}_${currentDate.getDate()}.json`;
+        // eslint-disable-next-line max-len
+        const filename = `..\\data\\logins.${currentDate.getFullYear()}_${currentDate.getMonth()+1}_${currentDate.getDate()}.json`;
 
         console.log(`reading ${filename}`);
 
         const data = JSON.parse(fs.readFileSync(filename));
-    
-        for(const account in data) {
-            if (!logins.hasOwnProperty(account)) {
-                logins[account] = [];
-                uniqueLogins++;    
-            }
 
-            data[account].forEach(a => {
-                if (!logins[account].some(l => (l.timestamp === a.timestamp) && (l.clientId === a.clientId))) {
-                    logins[account].push(a);
-                    totalLogins++
+        for (const account in data) {
+            if ({}.hasOwnProperty(data, account)) {
+                if (!logins.hasOwnProperty(account)) {
+                    logins[account] = [];
+                    uniqueLogins++;
                 }
-            });
-            //logins[account] = logins[account].concat(data[account]);
+
+                data[account].forEach((a) => {
+                    // eslint-disable-next-line max-len
+                    if (!logins[account].some((l) => (l.timestamp === a.timestamp) && (l.clientId === a.clientId))) {
+                        logins[account].push(a);
+                        totalLogins++;
+                    }
+                });
+            // logins[account] = logins[account].concat(data[account]);
+            }
         }
-        
+
         currentDate.setDate(currentDate.getDate() + 1);
     }
 
-    //fs.writeFileSync(`..\\data\\logins.total.json`, logins);
+    // fs.writeFileSync(`..\\data\\logins.total.json`, logins);
 
     console.log(`unique logins: ${uniqueLogins}`);
     console.log(`total logins: ${totalLogins}`);
 
-    var keyCount = 0;
+    let keyCount = 0;
     for (const account in logins) {
-        if (logins.hasOwnProperty(account)){
-
+        if (logins.hasOwnProperty(account)) {
             keyCount += logins[account].length;
         }
     }
 
     console.log(`key count: ${keyCount}`);
-
-
 })();
